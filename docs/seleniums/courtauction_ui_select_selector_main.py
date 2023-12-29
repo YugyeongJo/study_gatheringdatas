beefa4a9111667f4c105bf6cdbe19b9fdac1ba34
Yugyeong Jo <yugyeongjo1215@gmail.com>
2023-12-29 14:07:21 +0900

    add

---
+++ docs/seleniums/courtauction_ui_select_selector_main.py
@@ -0,0 +1,23 @@
+import courtauction_ui_select_selector_subfunction as subfunction
+
+# 기본 function 형식 - 기다림. 불리울 때 기능한다.
+
+def main():
+    try: 
+        uri="https://www.courtauction.go.kr/"
+        browser = subfunction.getBrowserFromURI(uri)        # 업무 코드 부분
+        court_count = subfunction.selectCourts(browser=browser)
+        print("court count : {}".format(court_count))
+    except:
+        pass        # 업무 코드 부분 문제 발생 시 대처 코드
+    finally:
+        subfunction.quitBrowser(browser=browser)        # try나 except이 끝난 후 무조건 실행 코드   
+    return 0
+
+if __name__ == "__main__":
+    try: 
+        main()        # 업무 코드 부분
+    except:
+        pass        # 업무 코드 부분가 문제 발생 시 대처 코드
+    finally:
+        pass        # try나 except이 끝난 후 무조건 실행 코드   
